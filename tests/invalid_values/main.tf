a90e80c39a44380cf16df4313810f07fe79e9260
Lev Gorbunov <lev.gorbunov@justice.gov.uk>
2026-04-28 13:59:38 +0100

    separate cicd checks into separate workflows and refactor terraform tests to be actual terraform code

---
+++ tests/invalid_values/main.tf
@@ -0,0 +1,30 @@
+terraform {
+    required_providers {
+        aws = {
+            source  = "hashicorp/aws"
+            version = "~> 5.0"
+        }
+    }
+}
+
+provider "aws" {
+    region = "eu-west-2"
+    skip_credentials_validation = true
+    skip_metadata_api_check     = true
+    skip_requesting_account_id  = true
+    access_key                  = "mock"
+    secret_key                  = "mock"
+}
+
+resource "aws_s3_bucket" "test" {
+    bucket = "test-bucket-compliant"
+
+    tags = {
+        business-unit = "HQ"
+        application   = "test-app"
+        owner         = "test@example.com"
+        is-production = "lalala"
+        service-area  = "testing"
+        environment   = "development"
+    }
+}
