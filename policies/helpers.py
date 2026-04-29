e18ba5cb8d7c3de511c18bfc7b3ced8c83174e0f
Lev Gorbunov <lev.gorbunov@justice.gov.uk>
2026-04-29 10:50:39 +0100

    convert value to string during unwrap

--- policies/helpers.py
+++ policies/helpers.py
@@ -1,6 +1,6 @@
 def unwrap(value):
     if isinstance(value, list):
-        return value[0] if value else None
+        return str(value[0]) if value else None
     return value
 
 def is_empty(value):
