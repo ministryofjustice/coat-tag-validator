2eeda9b3ab844af181fe42f6cb0579aac0babcbd
Lev Gorbunov <lev.gorbunov@justice.gov.uk>
2026-05-20 17:49:10 +0100

    linting

--- policies/required_tags.py
+++ policies/required_tags.py
@@ -28,12 +28,12 @@ class RequiredTagsCheck(BaseResourceCheck):
         # Account for untaggable resources - will not have a tags key
         if not tags:
             return CheckResult.PASSED
-        
+
         # Account for completely empty tag set
         if tags == [None]:
             tags = []
 
-        # use tags_all as ultimate source of tags, 
+        # use tags_all as ultimate source of tags,
         # unless tags is populated and tags_all is not (edge case)
         effective_tags = tags_all if tags_all else tags
 
